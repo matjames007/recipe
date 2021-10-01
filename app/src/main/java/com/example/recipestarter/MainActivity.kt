23ea92e74221d3bbe04f4b58d56dbbaa4993b957
Matthew Budram <matjames007@gmail.com>
2021-09-30 22:15:51 -0500

    Made all the changes necessary for the final version of the application

--- app/src/main/java/com/example/recipestarter/MainActivity.kt
+++ app/src/main/java/com/example/recipestarter/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.recipestarter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var listView: ListView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        listView = findViewById<ListView>(R.id.recipe_list_view)
+
+        val recipeList = Recipe.getRecipesFromFile("recipes.json", this)
+
+        val adapter = RecipeAdapter(this, recipeList)
+        listView.adapter = adapter
+
+        val context = this
+        listView.setOnItemClickListener { _, _, position, _ ->
+            val selectedRecipe = recipeList[position]
+
+            val detailIntent = RecipeDetailActivity.newIntent(context, selectedRecipe)
+
+            startActivity(detailIntent)
+        }
     }
 }
